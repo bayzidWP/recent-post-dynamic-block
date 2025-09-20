3f2d4ec23f7d193e8fc60cf2e626eeb0d88d29cf
bayzid-dev <sejan.bayzid@gmail.com>
2025-09-20 07:03:50 +0600

    Nothing to commit

--- recent-posts-showcase.php
+++ recent-posts-showcase.php
@@ -36,8 +36,6 @@ function create_block_recent_posts_showcase_block_init() {
 	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
 	 * based on the registered block metadata.
 	 * Added in WordPress 6.8 to simplify the block metadata registration process added in WordPress 6.7.
-	 *
-	 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
 	 */
 	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
 		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
@@ -47,8 +45,6 @@ function create_block_recent_posts_showcase_block_init() {
 	/**
 	 * Registers the block(s) metadata from the `blocks-manifest.php` file.
 	 * Added to WordPress 6.7 to improve the performance of block type registration.
-	 *
-	 * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
 	 */
 	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
 		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
@@ -56,7 +52,6 @@ function create_block_recent_posts_showcase_block_init() {
 	/**
 	 * Registers the block type(s) in the `blocks-manifest.php` file.
 	 *
-	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
 	 */
 	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
 	foreach ( array_keys( $manifest_data ) as $block_type ) {
